eb5c7e5a476474a17e18ad8d3b13720e55fab076
José Moreira <moreira1975@gmail.com>
2025-04-25 12:45:03 +0000

    Xadrez Nível Novato

---
+++ xadrez.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    // Simulação do movimento da Torre (usando for)
+    printf("Movimento da Torre:\n");
+    for (int i = 0; i < 5; i++) {
+        printf("Direita\n");
+    }
+    printf("\n");
+
+    // Simulação do movimento do Bispo (usando while)
+    printf("Movimento do Bispo:\n");
+    int j = 0;
+    while (j < 5) {
+        printf("Cima Direita\n");
+        j++;
+    }
+    printf("\n");
+
+    // Simulação do movimento da Rainha (usando do-while)
+    printf("Movimento da Rainha:\n");
+    int k = 0;
+    do {
+        printf("Esquerda\n");
+        k++;
+    } while (k < 8);
+    printf("\n");
+
+    return 0;
+}
